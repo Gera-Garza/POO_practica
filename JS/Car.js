339fea1e398d0cfc667e9b3d69130a7365d51ee9
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-24 20:19:52 -0500

    reestructuracion de las clases de js segun nuevo formato EcmaScript 6

--- JS/Car.js
+++ JS/Car.js
@@ -1,12 +1,15 @@
-function Car(license,driver){
-    this.id;
-    this.license = license;
-    this.driver = driver;
-    this.passanger;
-}
-Car.prototype.printDataCar = function(){
-    console.log(this.driver)
-    console.log(this.driver.name)
-    console.log(this.driver.document)
+class Car{
+    constructor(license,driver){
+        this.id;
+        this.license = license;
+        this.driver = driver;
+        this.passanger;
+    }
 
+    printDataCar(){
+        console.log(this.driver)
+        console.log(this.driver.name)
+        console.log(this.driver.document)
+    }
+   
 }
