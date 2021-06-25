2bce3e42c6d4bc0f6af3bf607967db346c0f151a
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-24 20:15:04 -0500

    mejoramiento del java y creacion de objetos en js

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,13 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        Car car = new Car("AMQ123",new Account("Andres Herrera", "AND123"));
+        car.passanger=4;
+        car.printDataCar();
+
+        Car car2 = new Car("AMQ456",new Account("Maria Garcia", "AND456"));
+        car2.passanger=2;
+        car2.printDataCar();
     }
 }
  
