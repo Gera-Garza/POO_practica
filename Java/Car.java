0ddea5ee898621b46ef62973513f6865ada54880
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-24 22:27:40 -0500

    encapsulamiento de passanger en car

--- Java/Car.java
+++ Java/Car.java
@@ -2,13 +2,21 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passanger;
+    private Integer passanger;
     
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
     }
     void printDataCar(){
-        System.out.println("License: " + license + " Driver: " + driver.name);
+        System.out.println("License: " + license + " Driver: " + driver.name + " Passangers: " + passanger);
+    }
+
+    public Integer getPassanger(){
+        return passanger;
+    }
+
+    public void setPassanger(Integer passanger){
+        this.passanger = passanger;
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,13 +1,14 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        Car car = new Car("AMQ123",new Account("Andres Herrera", "AND123"));
-        car.passanger=4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123",new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
+        //uberX.passanger = 4;
+        uberX.setPassanger(4);
+        uberX.printDataCar();
 
-        Car car2 = new Car("AMQ456",new Account("Maria Garcia", "AND456"));
+        /*Car car2 = new Car("AMQ456",new Account("Maria Garcia", "AND456"));
         car2.passanger=2;
-        car2.printDataCar();
+        car2.printDataCar();*/
     }
 }
  
